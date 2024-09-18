fedbffee79773aeae8db4b02a15b061d77df94ec
Yakiba1 <Maycop.gq@gmail.com>
2024-09-18 14:33:42 +0500

    add yml

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
-<template>
-  <div>
+<script setup lang="ts">
+const d = 4
+function count(f: number) {
+  return d + f
+}
+console.log(count(8))
 
+const todoList = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+</script>
+
+
+<template>
+  <div class="container">
+    <div> ХУЕЦ</div>
+    <test-es-lint/>
+    <testEsLint :listitems="todoList"/>
   </div>
 </template>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
+
