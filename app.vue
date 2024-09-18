51b638e30cd518915ee9e5bec37eef731cc60156
Yakiba1 <Maycop.gq@gmail.com>
2024-09-18 15:58:19 +0500

    fixNamesa

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import TestCi from "~/components/testCi.vue";
+
 const d = 4
 function count(f: number) {
   return d + f
@@ -15,6 +17,7 @@ const todoList = ["a", "b", "c", "d", "e", "f", "g", "h"]
     <div> ХУЕЦ</div>
     <test-es-lint/>
     <testEsLint :listitems="todoList"/>
+    <test-ci/>
   </div>
 </template>
 
